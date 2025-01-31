e3315f77c92477d8cb9834439f2d55b8a80e42c1
Proful <proful.sadangi@gmail.com>
2025-02-01 05:00:21 +0530

    mul near perfect

--- app/mul.tsx
+++ app/mul.tsx
@@ -6,6 +6,7 @@ import {
   Group,
   Path,
   interpolate,
+  SkFont,
 } from "@shopify/react-native-skia";
 import {
   useDerivedValue,
@@ -14,6 +15,7 @@ import {
   withSequence,
   withSpring,
   withTiming,
+  SharedValue,
 } from "react-native-reanimated";
 import { useCallback, useEffect, useState } from "react";
 import { useFocusEffect } from "expo-router";
@@ -21,44 +23,317 @@ import {
   configureReanimatedLogger,
   ReanimatedLogLevel,
 } from "react-native-reanimated";
-import { l } from "@/lib/utils";
 
-// This is the default configuration
+// Configure Reanimated Logger
 configureReanimatedLogger({
   level: ReanimatedLogLevel.warn,
-  strict: false, // Reanimated runs in strict mode by default
+  strict: false,
 });
-type Step = {
+
+// Constants
+const CONSTANTS = {
+  SPACING: 50,
+  BASE_X: 150,
+  BASE_Y: 120,
+  STEP_HEIGHT: 80,
+  LINE_WIDTH: 200,
+  STEP_DELAY: 1000,
+  ANIMATION_DURATION: 2000,
+} as const;
+
+// Types
+interface Step {
   value: number;
   carry: number;
   writeDown: number;
   multiplierDigit: number;
   multiplicandDigit: number;
   shift: number;
-  x: number; // New property for x position
-  y: number; // New property for y position
-};
+  x: number;
+  y: number;
+}
 
-const SPACING = 50;
-const BASE_X = 150;
-const BASE_Y = 120;
-const STEP_HEIGHT = 80;
-const LINE_WIDTH = 200;
-
-const STEP_DELAY = 1000;
-const ANIMATION_DURATION = 2000;
-
-type MultiplicationAnimatorProps = {
+interface MultiplicationAnimatorProps {
   multiplicand?: number;
   multiplier?: number;
+}
+
+interface MultiplicationDisplayProps {
+  multiplicand: number;
+  multiplier: number;
+  font: SkFont;
+}
+
+interface StepsDisplayProps {
+  steps: Step[];
+  currentStep: number;
+  derivedValues: DerivedValues;
+  font: SkFont;
+}
+
+interface StepCalculationProps {
+  text: string;
+  x: number;
+  y: number;
+  font: SkFont;
+  opacity: SharedValue<number>;
+}
+
+interface StepValuesProps {
+  step: Step;
+  prevCarry?: number;
+  writeDownX: SharedValue<number>;
+  writeDownY: SharedValue<number>;
+  opacity: SharedValue<number>;
+  carryOpacity: SharedValue<number>;
+  font: SkFont;
+}
+
+interface DerivedValues {
+  opacities: SharedValue<number>[];
+  opacitiesForCarry: SharedValue<number>[];
+  xWriteDown: SharedValue<number>[];
+  yWriteDown: SharedValue<number>[];
+}
+
+// Helper Functions
+const getCalculationText = (step: Step, prevCarry?: number): string => {
+  if (!step.multiplicandDigit) return "";
+
+  if (prevCarry && prevCarry > 0) {
+    return `${step.multiplierDigit}×${step.multiplicandDigit}+${prevCarry}=${step.multiplicandDigit * step.multiplierDigit}+${prevCarry}=${step.multiplicandDigit * step.multiplierDigit + prevCarry}`;
+  }
+
+  return `${step.multiplierDigit}×${step.multiplicandDigit}=${step.multiplicandDigit * step.multiplierDigit}`;
 };
 
-const MultiplicationAnimator = ({
+const calculatePositionForStep = (
+  shift: number,
+  stepsLength: number,
+  currentSteps: Step[],
+  multiplicandLength: number,
+  multiplierLength: number,
+): { x: number; y: number } => {
+  const len = multiplicandLength * multiplierLength;
+  const iOffset =
+    shift === 0 ? len - currentSteps.length - 1 : len - currentSteps.length + 2;
+
+  return {
+    x: CONSTANTS.BASE_X - 150 + iOffset * CONSTANTS.SPACING,
+    y: CONSTANTS.BASE_Y + 50 + shift * CONSTANTS.STEP_HEIGHT,
+  };
+};
+
+const calculateSteps = (multiplicand: number, multiplier: number): Step[] => {
+  const multiplicandStr = multiplicand.toString();
+  const multiplierStr = multiplier.toString();
+  const steps: Step[] = [];
+
+  for (let i = multiplierStr.length - 1; i >= 0; i--) {
+    const shift = multiplierStr.length - 1 - i;
+    let carry = 0;
+
+    for (let j = multiplicandStr.length - 1; j >= 0; j--) {
+      const multiplierDigit = parseInt(multiplierStr[i]);
+      const multiplicandDigit = parseInt(multiplicandStr[j]);
+      const product = multiplierDigit * multiplicandDigit + carry;
+
+      const { x, y } = calculatePositionForStep(
+        shift,
+        steps.length,
+        steps,
+        multiplicandStr.length,
+        multiplierStr.length,
+      );
+
+      steps.push({
+        value: product,
+        carry: Math.floor(product / 10),
+        writeDown: product % 10,
+        multiplierDigit,
+        multiplicandDigit,
+        shift,
+        x,
+        y,
+      });
+
+      carry = Math.floor(product / 10);
+    }
+
+    if (carry > 0) {
+      const { x, y } = calculatePositionForStep(
+        shift,
+        steps.length,
+        steps,
+        multiplicandStr.length,
+        multiplierStr.length,
+      );
+
+      steps.push({
+        value: carry,
+        carry: 0,
+        writeDown: carry,
+        multiplierDigit: parseInt(multiplierStr[i]),
+        multiplicandDigit: 0,
+        shift,
+        x,
+        y,
+      });
+    }
+  }
+
+  return steps;
+};
+
+// SubComponents
+const StepCalculation: React.FC<StepCalculationProps> = ({
+  text,
+  x,
+  y,
+  font,
+  opacity,
+}) => <SkiaText x={x} y={y} text={text} font={font} opacity={opacity} />;
+
+const StepValues: React.FC<StepValuesProps> = ({
+  step,
+  prevCarry,
+  writeDownX,
+  writeDownY,
+  opacity,
+  carryOpacity,
+  font,
+}) => (
+  <Group>
+    <SkiaText
+      x={80}
+      y={sh / 2 + 40}
+      text={(step.value % 10).toString()}
+      font={font}
+      opacity={carryOpacity}
+    />
+    {step.carry > 0 && (
+      <SkiaText
+        x={80}
+        y={sh / 2 + 70}
+        text={`Current Carry: ${step.carry}`}
+        font={font}
+        opacity={carryOpacity}
+      />
+    )}
+    {prevCarry !== undefined && (
+      <SkiaText
+        x={80}
+        y={sh / 2 + 100}
+        text={`Prev Carry: ${prevCarry}`}
+        font={font}
+        opacity={carryOpacity}
+      />
+    )}
+    <SkiaText
+      x={writeDownX}
+      y={writeDownY}
+      text={step.writeDown.toString()}
+      font={font}
+      opacity={opacity}
+    />
+  </Group>
+);
+
+const MultiplicationDisplay: React.FC<MultiplicationDisplayProps> = ({
+  multiplicand,
+  multiplier,
+  font,
+}) => (
+  <Group>
+    {String(multiplicand)
+      .split("")
+      .map((char, i) => (
+        <SkiaText
+          key={`multiplicand-${i}`}
+          x={CONSTANTS.BASE_X + i * 40}
+          y={CONSTANTS.BASE_Y - 40}
+          text={char}
+          font={font}
+        />
+      ))}
+    {String(multiplier)
+      .split("")
+      .map((char, i) => (
+        <SkiaText
+          key={`multiplier-${i}`}
+          x={CONSTANTS.BASE_X + i * 40}
+          y={CONSTANTS.BASE_Y}
+          text={char}
+          font={font}
+        />
+      ))}
+    <SkiaText
+      x={CONSTANTS.BASE_X - 30}
+      y={CONSTANTS.BASE_Y}
+      text="×"
+      font={font}
+    />
+    <Path
+      path={`M ${CONSTANTS.BASE_X - 30} ${CONSTANTS.BASE_Y + 20} h ${
+        CONSTANTS.LINE_WIDTH
+      }`}
+      strokeWidth={2}
+      style="stroke"
+      color="#000"
+    />
+  </Group>
+);
+
+const StepsDisplay: React.FC<StepsDisplayProps> = ({
+  steps,
+  currentStep,
+  derivedValues,
+  font,
+}) => (
+  <Group>
+    {steps.map((step, i) => {
+      if (i > currentStep) return null;
+
+      const calcText = getCalculationText(step, steps[i - 1]?.carry);
+
+      return (
+        <Group key={`step-${i}`}>
+          <StepCalculation
+            text={calcText}
+            x={80}
+            y={sh / 2 + 10}
+            font={font}
+            opacity={derivedValues.opacitiesForCarry[i]}
+          />
+          <StepValues
+            step={step}
+            prevCarry={steps[i - 1]?.carry}
+            writeDownX={derivedValues.xWriteDown[i]}
+            writeDownY={derivedValues.yWriteDown[i]}
+            opacity={derivedValues.opacities[i]}
+            carryOpacity={derivedValues.opacitiesForCarry[i]}
+            font={font}
+          />
+        </Group>
+      );
+    })}
+    <Path
+      path={`M ${CONSTANTS.BASE_X - 30} ${
+        CONSTANTS.BASE_Y + 20 + CONSTANTS.STEP_HEIGHT * 2
+      } h ${CONSTANTS.LINE_WIDTH}`}
+      strokeWidth={2}
+      style="stroke"
+      color="#000"
+    />
+  </Group>
+);
+
+// Main Component
+const MultiplicationAnimator: React.FC<MultiplicationAnimatorProps> = ({
   multiplicand = 85,
   multiplier = 94,
-}: MultiplicationAnimatorProps) => {
-  const steps = cal();
-  // const [steps, setSteps] = useState<Step[]>([]);
+}) => {
+  const steps = calculateSteps(multiplicand, multiplier);
   const [currentStep, setCurrentStep] = useState(-1);
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -91,6 +366,14 @@ const MultiplicationAnimator = ({
       );
     }),
   );
+
+  const derivedValues = {
+    opacities: derivedOpacities,
+    opacitiesForCarry: derivedOpacitiesForCarry,
+    xWriteDown: xWriteDownValues,
+    yWriteDown: yWriteDownValues,
+  };
+
   const font = useFont(require("../assets/BlexMonoNerdFont-Regular.ttf"), 24);
 
   const setup = useCallback(() => {
@@ -116,23 +399,26 @@ const MultiplicationAnimator = ({
 
       // Animate opacitiesForCarry
       opacitiesForCarry[stepIndex].value = withSequence(
-        withDelay(STEP_DELAY, withTiming(1, { duration: ANIMATION_DURATION })), // Fade in
-        withTiming(0, { duration: ANIMATION_DURATION }), // Fade out
+        withDelay(
+          CONSTANTS.STEP_DELAY,
+          withTiming(1, { duration: CONSTANTS.ANIMATION_DURATION }),
+        ), // Fade in
+        withTiming(0, { duration: CONSTANTS.ANIMATION_DURATION }), // Fade out
       );
 
       // Animate opacities
       opacities[stepIndex].value = withSequence(
         withDelay(
-          STEP_DELAY + ANIMATION_DURATION,
-          withTiming(1, { duration: ANIMATION_DURATION }),
+          CONSTANTS.STEP_DELAY + CONSTANTS.ANIMATION_DURATION,
+          withTiming(1, { duration: CONSTANTS.ANIMATION_DURATION }),
         ), // Fade in
         withSpring(1), // Stay visible
       );
 
       xy[stepIndex].value = withSequence(
         withDelay(
-          STEP_DELAY + ANIMATION_DURATION,
-          withTiming(1, { duration: ANIMATION_DURATION }),
+          CONSTANTS.STEP_DELAY + CONSTANTS.ANIMATION_DURATION,
+          withTiming(1, { duration: CONSTANTS.ANIMATION_DURATION }),
         ), // Fade in
       );
       // Mark animation as complete after all animations finish
@@ -140,7 +426,7 @@ const MultiplicationAnimator = ({
         () => {
           setIsAnimating(false);
         },
-        STEP_DELAY + 2 * ANIMATION_DURATION,
+        CONSTANTS.STEP_DELAY + 2 * CONSTANTS.ANIMATION_DURATION,
       ); // Total duration for this step
     },
     [currentStep],
@@ -155,140 +441,28 @@ const MultiplicationAnimator = ({
     }
   };
 
-  if (!font) {
-    return <View style={styles.container} />;
-  }
-
-  const renderMultiplication = () => (
-    <Group>
-      {/* Multiplicand */}
-      {multiplicand
-        .toString()
-        .split("")
-        .map((char, i) => (
-          <SkiaText
-            key={i}
-            x={BASE_X + i * 40}
-            y={BASE_Y - 40}
-            text={char}
-            font={font}
-          />
-        ))}
-
-      {/* Multiplier */}
-      {multiplier
-        .toString()
-        .split("")
-        .map((char, i) => (
-          <SkiaText
-            key={i}
-            x={BASE_X + i * 40}
-            y={BASE_Y}
-            text={char}
-            font={font}
-          />
-        ))}
-
-      {/* Multiplication symbol */}
-      <SkiaText x={BASE_X - 30} y={BASE_Y} text="×" font={font} />
-
-      {/* Horizontal line */}
-      <Path
-        path={`M ${BASE_X - 30} ${BASE_Y + 20} h ${LINE_WIDTH}`}
-        strokeWidth={2}
-        style="stroke"
-        color="#000"
-      />
-    </Group>
-  );
-
-  const renderSteps = () => (
-    <Group>
-      {steps.map((step, stepIndex) => {
-        if (stepIndex > currentStep) return null;
-
-        let val = "";
-
-        if (
-          stepIndex > 0 &&
-          steps[stepIndex - 1].carry > 0 &&
-          step.multiplicandDigit > 0
-        ) {
-          val = `${step.multiplierDigit}X${step.multiplicandDigit}+${steps[stepIndex - 1].carry}=${step.multiplicandDigit * step.multiplierDigit}+${steps[stepIndex - 1].carry}=${step.multiplicandDigit * step.multiplierDigit + steps[stepIndex - 1].carry}`;
-        } else if (step.multiplicandDigit > 0) {
-          val = `${step.multiplierDigit}X${step.multiplicandDigit}=${step.multiplicandDigit * step.multiplierDigit}`;
-        } else {
-          val = "";
-        }
-
-        return (
-          <Group key={`step-${stepIndex}`}>
-            <SkiaText
-              x={80}
-              y={sh / 2 + 10}
-              text={val}
-              font={font}
-              opacity={derivedOpacitiesForCarry[stepIndex]}
-            />
-            <SkiaText
-              x={80}
-              y={sh / 2 + 40}
-              text={(step.value % 10) + ""}
-              font={font}
-              opacity={derivedOpacitiesForCarry[stepIndex]}
-            />
-            {/* Carry number (if exists) */}
-            {step.carry > 0 && (
-              <SkiaText
-                x={80}
-                y={sh / 2 + 70}
-                text={`Current Carry: ${step.carry.toString()}`}
-                font={font}
-                opacity={derivedOpacitiesForCarry[stepIndex]}
-              />
-            )}
-            {stepIndex > 0 && (
-              <SkiaText
-                x={80}
-                y={sh / 2 + 100}
-                text={`Prev Carry: ${steps[stepIndex - 1].carry.toString()}`}
-                font={font}
-                opacity={derivedOpacitiesForCarry[stepIndex]}
-              />
-            )}
-
-            {/* Write down number */}
-            <SkiaText
-              x={xWriteDownValues[stepIndex].value} // Use the precomputed x position
-              y={yWriteDownValues[stepIndex].value} // Use the precomputed y position
-              text={step.writeDown.toString()}
-              font={font}
-              opacity={derivedOpacities[stepIndex]}
-            />
-          </Group>
-        );
-      })}
-
-      {/* Final horizontal line */}
-      <Path
-        path={`M ${BASE_X - 30} ${BASE_Y + 20 + STEP_HEIGHT * 2} h ${LINE_WIDTH}`}
-        strokeWidth={2}
-        style="stroke"
-        color="#000"
-      />
-    </Group>
-  );
+  if (!font) return <View style={styles.container} />;
 
   return (
     <View style={styles.container}>
       <Canvas style={styles.canvas}>
-        {renderMultiplication()}
-        {renderSteps()}
+        <MultiplicationDisplay
+          multiplicand={multiplicand}
+          multiplier={multiplier}
+          font={font}
+        />
+        <StepsDisplay
+          steps={steps}
+          currentStep={currentStep}
+          derivedValues={derivedValues}
+          font={font}
+        />
       </Canvas>
     </View>
   );
 };
 
+// Styles
 const { width: sw, height: sh } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
@@ -303,63 +477,3 @@ const styles = StyleSheet.create({
 });
 
 export default MultiplicationAnimator;
-
-const cal = () => {
-  const multiplicandStr = "85";
-  const multiplierStr = "94";
-  const newSteps: Step[] = [];
-
-  // Calculate each step of multiplication
-  for (let i = multiplierStr.length - 1; i >= 0; i--) {
-    const shift = multiplierStr.length - 1 - i;
-    let carry = 0;
-
-    for (let j = multiplicandStr.length - 1; j >= 0; j--) {
-      const multiplierDigit = parseInt(multiplierStr[i]);
-      const multiplicandDigit = parseInt(multiplicandStr[j]);
-
-      const product = multiplierDigit * multiplicandDigit + carry;
-      const writeDown = product % 10;
-      carry = Math.floor(product / 10);
-
-      // Calculate x and y positions for writeDown
-      const len = multiplicandStr.length * multiplierStr.length;
-      const iOffset =
-        shift === 0 ? len - newSteps.length - 1 : len - newSteps.length + 2;
-      const xOffset = BASE_X - 150 + iOffset * SPACING;
-      const yOffset = BASE_Y + 50 + shift * STEP_HEIGHT;
-
-      newSteps.push({
-        value: product,
-        carry,
-        writeDown,
-        multiplierDigit,
-        multiplicandDigit,
-        shift,
-        x: xOffset, // Store x position
-        y: yOffset, // Store y position
-      });
-    }
-
-    // Handle final carry if exists
-    if (carry > 0) {
-      const len = multiplicandStr.length * multiplierStr.length;
-      const iOffset =
-        shift === 0 ? len - newSteps.length - 1 : len - newSteps.length + 2;
-      const xOffset = BASE_X - 150 + iOffset * SPACING;
-      const yOffset = BASE_Y + 50 + shift * STEP_HEIGHT;
-
-      newSteps.push({
-        value: carry,
-        carry: 0,
-        writeDown: carry,
-        multiplierDigit: parseInt(multiplierStr[i]),
-        multiplicandDigit: 0,
-        shift,
-        x: xOffset, // Store x position
-        y: yOffset, // Store y position
-      });
-    }
-  }
-  return newSteps;
-};
